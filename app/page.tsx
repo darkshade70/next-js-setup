a76c8d3528774cbfcc67e6b619cae454ac1f7e33
jeff li <jeff-li@live.ca>
2024-11-26 09:44:00 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="py-10">
+      <div className="container">
+        <h1 className="text-3x1 font-bold">Hello World</h1>
+      </div>
+    </section>
+  );
+}
